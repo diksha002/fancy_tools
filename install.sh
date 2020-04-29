25ece3f6941c60918899d0aed68580aa2038a62e
diksha002 <diksha.raghoonundun@gmail.com>
2020-04-29 18:49:53 +0400

    [master] ajouter bin au PATH

--- install.sh
+++ install.sh
@@ -4,15 +4,26 @@ dir=$(pwd)
 a="source $dir/.aliases"
 b="source $dir/fancy_functions.sh"
 c=$(<~/.bashrc)
+d="export PATH=$PATH:$dir/bin"
 
 if grep -q "$a" <<< "$c"; then
 	echo "File '.aliases' already in bash"
 else
 	echo source $dir/.aliases >> ~/.bashrc
+	echo "File '.aliases' successfully added in bash"
 fi
 
 if grep -q "$b" <<< "$c"; then
 	echo "File 'Fancy_functions.sh' already in bash"
 else
 	echo source $dir/fancy_functions.sh >> ~/.bashrc
+	echo "File 'fancy_functions.sh' successfully added in bash"
 fi
+
+if grep -q "$d" <<< "$c"; then
+	echo "PATH already added in bash"
+else
+	echo "export PATH=$PATH:$dir/bin" >> ~/.bashrc
+	echo "PATH successfully added in bash"
+fi
+
