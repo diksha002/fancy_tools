c4b0d0c4b24fe6f6521de325e26d2c03e88e7ab0
diksha002 <diksha.raghoonundun@gmail.com>
2020-06-24 22:49:46 +0400

    install.sh final

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-dir=$(pwd)
+dir="~/src/fancy_tools"
 a="source $dir/.aliases"
 b="source $dir/fancy_functions.sh"
 c=$(<~/.bashrc)
-d="export PATH=$PATH:$dir/bin"
+d="export PATH=$PATH:~/bin"
 
+#file .aliases
 if grep -q "$a" <<< "$c"; then
 	echo "File '.aliases' already in bash"
 else
@@ -13,6 +14,7 @@ else
 	echo "File '.aliases' successfully added in bash"
 fi
 
+#file fancy_functions.sh
 if grep -q "$b" <<< "$c"; then
 	echo "File 'Fancy_functions.sh' already in bash"
 else
@@ -20,10 +22,40 @@ else
 	echo "File 'fancy_functions.sh' successfully added in bash"
 fi
 
+#folder ~/bin
+if [[ -d ~/bin ]]
+then
+	echo "folder ~/bin already exists"
+else
+	mkdir ~/bin
+	echo "folder ~/bin successfully created"
+fi
+
+#PATH
 if grep -q "$d" <<< "$c"; then
 	echo "PATH already added in bash"
 else
-	echo "export PATH=$PATH:$dir/bin" >> ~/.bashrc
+	echo "export PATH=$PATH:~/bin" >> ~/.bashrc
 	echo "PATH successfully added in bash"
 fi
 
+#folder src
+if [[ -d ~/src ]]
+then
+        echo "folder ~/src already exists"
+else
+        mkdir ~/src
+        echo "folder ~/src successfully created"
+fi
+
+#clone fancy_tools in src
+if [[ -d ~/src/fancy_tools ]]
+then
+	echo "fancy_tools already cloned in folder src"
+else
+	git clone --single-branch --branch master https://github.com/diksha002/fancy_tools.git ~/src/fancy_tools
+	echo "successfully cloned fancy_tools in ~/src"
+fi
+
+cp ~/src/fancy_tools/bin/updateFancyTools ~/bin
+chmod +x ~/bin/updateFancyTools
